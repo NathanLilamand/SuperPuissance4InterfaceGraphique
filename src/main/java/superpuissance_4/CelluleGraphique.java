3ed6e95e28ff7c0dd4055abb0fcbc64ff7f2174a
nathan lilamand <nath.lilamand@gmail.com>
2020-11-21 21:24:42 +0100

    set initialiser_partie() on fenetredejeux.java an set different image to a special type of celluleAssociee

--- src/main/java/superpuissance_4/CelluleGraphique.java
+++ src/main/java/superpuissance_4/CelluleGraphique.java
@@ -11,21 +11,43 @@ import javax.swing.JButton;
 
 /**
  *
- * @author Marc L
+ * @author Nathan L
  */
 public class CelluleGraphique extends JButton {
+
     Cellule celluleAssociee;
     // image a charger
     ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/images/celluleVide.png"));
-    
-    public CelluleGraphique (Cellule uneCellule){
-        celluleAssociee=uneCellule;
+    ImageIcon img_desint = new javax.swing.ImageIcon(getClass().getResource("/images/desintegrateur.png"));
+    ImageIcon img_jetonJaune = new javax.swing.ImageIcon(getClass().getResource("/images/jetonJaune.png"));
+    ImageIcon img_jetonRouge = new javax.swing.ImageIcon(getClass().getResource("/images/jetonRouge.png"));
+    ImageIcon img_trouNoir = new javax.swing.ImageIcon(getClass().getResource("/images/trouNoir.png"));
+
+    public CelluleGraphique(Cellule uneCellule) {
+        celluleAssociee = uneCellule;
     }
-    
+
     @Override
-    public void paintComponent (Graphics G) {
+    public void paintComponent(Graphics G) {
         super.paintComponent(G);
-        setIcon(img_vide); // on attribue cellulevide.png
-        
+        if (celluleAssociee.presenceTrouNoir()) {
+            setIcon(img_trouNoir); // on attribue images/trouNoir.png
+        } else if (celluleAssociee.presenceDesintegrateur()) {
+            setIcon(img_desint);
+        } else {
+            String couleur_jeton = celluleAssociee.lireCouleurDuJeton();
+            switch (couleur_jeton) {
+                case "":
+                    setIcon(img_vide); // on attribue cellulevide
+                    break;
+                case "Rouge":
+                    setIcon(img_jetonRouge);
+                    break;
+                case "Jaune":
+                    setIcon(img_jetonJaune);
+                    break;
+            }
+        }
+
     }
 }
